9a3d53bf57b4ae9d4a03bde6a1153873823578c8
gilguerra1 <111063426+gilguerra1@users.noreply.github.com>
2025-04-30 19:28:35 -0300

    Ajustes e adicionado testes

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,15 @@
 package main;
 
-//import view.OcorrenciaView;
+import view.OcorrenciaView;
 //import view.ApartamentoBlocoView;
-//import view.UsuarioView;
-
+import view.UsuarioView;
 import view.ComunicadoView;
 
 public class Main {
     public static void main(String[] args) {
         //ApartamentoBlocoView.ApartamentoBlocoMenu();
         //UsuarioView.UsuarioMenu();
-        //OcorrenciaView.menuOcorrencias();
-        ComunicadoView.menuComunicado();
+        OcorrenciaView.menuOcorrencias();
+        //ComunicadoView.menuComunicado();
     }
 }
