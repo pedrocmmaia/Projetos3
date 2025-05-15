258f85a0e16765630e6ccc8486cbccdb8d9010a5
JoaoPedro-Costa-Oliveira <168649576+JoaoPedro-Costa-Oliveira@users.noreply.github.com>
2025-05-15 17:30:32 -0300

    Trocando o case

--- src/main/java/view/MoradorView.java
+++ src/main/java/view/MoradorView.java
@@ -16,20 +16,18 @@ public class MoradorView {
             System.out.println("3. CRUD Reserva");
             System.out.println("4. Visualizar cobranças");
             System.out.println("5. Comunicados");
-            System.out.println("6. Apartamento Bloco");
             System.out.println("0. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
             scanner.nextLine();
 
             switch (opcao) {
-                case 1 -> System.out.println("Editar dados da conta de " + usuario.getNome());
+                case 1 -> UsuarioView.UsuarioMenu();
                 case 2 -> OcorrenciaView.menuOcorrencias();
-                case 3 -> System.out.println("Acessando CRUD de Reservas...");
-                case 4 -> System.out.println("Visualizando cobranças...");
+                //case 3 -> ReservasView.menuReservas();
+                //case 4 -> CobrancasView.menuCobrancas();
                 case 5 -> ComunicadoView.menuComunicado();
-                case 6 -> ApartamentoBlocoView.ApartamentoBlocoMenu();
-                case 0 -> System.out.println("Saindo...");
+                //case 0 -> System.out.println("Saindo...");
                 default -> System.out.println("Opção inválida.");
             }
         } while (opcao != 0);
