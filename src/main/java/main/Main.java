d63d48c4930780b5362f9f16ce821a09a0f1f227
gilguerra1 <111063426+gilguerra1@users.noreply.github.com>
2025-05-20 16:17:01 -0300

    Ajuste main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,17 +1,9 @@
 package main;
 
 import view.LoginView;
-import view.OcorrenciaView;
-import view.ApartamentoBlocoView;
-import view.UsuarioView;
-import view.ComunicadoView;
 
 public class Main {
     public static void main(String[] args) {
-        //ApartamentoBlocoView.ApartamentoBlocoMenu();
-        //UsuarioView.UsuarioMenu();
-        //OcorrenciaView.menuOcorrencias();
-        //ComunicadoView.menuComunicado();
         LoginView.loginMenu();
     }
 }
