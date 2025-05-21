e6b30d8f3c71138a102c9c1d8ef9f6a077387cff
Pedro Maia <pedrocmmaia@gmail.com>
2025-05-21 19:57:13 -0300

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,15 +1,9 @@
 package main;
 
-import view.OcorrenciaView;
-//import view.ApartamentoBlocoView;
-import view.UsuarioView;
-import view.ComunicadoView;
+import view.LoginView;
 
 public class Main {
     public static void main(String[] args) {
-        //ApartamentoBlocoView.ApartamentoBlocoMenu();
-        //UsuarioView.UsuarioMenu();
-        OcorrenciaView.menuOcorrencias();
-        //ComunicadoView.menuComunicado();
+        LoginView.loginMenu();
     }
 }
